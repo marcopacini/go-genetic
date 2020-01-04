49251b6160c4de39f35f01a2ecb250e2f9b7ab5a
Marco Pacini <marco.pacini@adevinta.com>
2020-01-04 01:13:04 +0100

    Add Preview component

--- example/image/ui/src/App.js
+++ example/image/ui/src/App.js
@@ -1,27 +1,75 @@
 import React from 'react';
-import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap';
+import {Button, Card, Col, Container, Image, Row, Spinner} from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Preview extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            started: false,
+            src: "http://localhost:3001"
+        };
+
+        this.handleStart= this.handleStart.bind(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        if (this.state.started) {
+            this.setState({
+                src: "http://localhost:3001?date=" + new Date()
+            })
+        }
+    }
+
+    handleStart() {
+        this.setState(state => ({
+            started: !state.started
+        }));
+    }
+
+    render() {
+        return (
+            <Card>
+                <Card.Header>Genetic Art, powered by <i>go-genetic</i></Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        <b>n.b.</b> <i>working in progress</i>
+                        <br /><br />
+                        <Image src={this.state.src} rounded />
+                        <br /><br />
+                        <Button
+                            onClick={this.handleStart}
+                            disabled={this.state.started}
+                            variant="primary"
+                            size="xl"
+                        >
+                            Start
+                        </Button>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
 
 function App() {
   return (
     <Container className="h-100">
       <Row className="h-100 justify-content-center align-items-center text-center">
           <Col lg={6}>
-          <Card>
-            <Card.Header>Genetic Art, powered by <i>go-genetic</i></Card.Header>
-            <Card.Body>
-              <Card.Text>
-                <b>n.b.</b> <i>working in progress</i>
-                <br /><br />
-                <Image src="http://localhost:3001" rounded />
-                <br /><br />
-              </Card.Text>
-              <Button variant="primary">
-                Generate
-              </Button>
-            </Card.Body>
-          </Card>
+            <Preview />
           </Col>
       </Row>
     </Container>
